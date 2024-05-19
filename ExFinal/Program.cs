e5838a935c56f0ffebe7128cd714be31980d1713
CarloJunior06 <manuelcocojunior06@gmail.com>
2024-05-19 09:02:24 +0200

    implementing database logic

--- ExFinal/Program.cs
+++ ExFinal/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ExFinal.models;
 using ExFinal.reader;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Digests;
 
 namespace ExFinal
 {
@@ -7,21 +9,33 @@ namespace ExFinal
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=localhost;Database=SdominoDatabase;User ID= Kvaraskhelia\\manue;Password=1926;SslMode=none;";
+
             string filePath = "ordini.csv";
             string fullPath = Path.GetFullPath(filePath);
             Console.WriteLine($"Percorso completo del file: {fullPath}");
             CSVReader reader = new CSVReader(filePath);
             List<OrdinePizza> ordini = reader.ReadCSV();
-
-            foreach (var ordine in ordini)
+            using (var connection = new MySqlConnection(connectionString))
             {
-                float prezzoTotale = ordine.BasePizza.Prezzo + ordine.Impasto.Prezzo  + ordine.Aggiunte.Sum(a => a.Nome == "Ananas" ? 0.0f : a.Prezzo);
-                if (ordine.Aggiunte.Any(a => a.Nome == "Ananas"))
+                connection.Open();
+                foreach (var ordine in ordini)
                 {
-                    prezzoTotale = 0.0f;
+                    float prezzoTotale = ordine.BasePizza.Prezzo + ordine.Impasto.Prezzo + ordine.Aggiunte.Sum(a => a.Nome == "Ananas" ? 0.0f : a.Prezzo);
+                    if (ordine.Aggiunte.Any(a => a.Nome == "Ananas"))
+                    {
+                        prezzoTotale = 0.0f;
+                    }
+                    string basePizza = ordine.BasePizza.Nome;
+                    string impasto = ordine.Impasto.Nome;
+                    string aggiunte = string.Join(", ", ordine.Aggiunte.Select(a => a.Nome));
+                    int idScontrino = ordine.IdScontrino;
+                    Console.WriteLine($"Base: {ordine.BasePizza.Nome}, Impasto: {ordine.Impasto.Nome}, Aggiunte: {string.Join(", ", ordine.Aggiunte.Select(a => a.Nome))}, ID Ordine: {ordine.IdScontrino}, Prezzo Totale: {prezzoTotale}");
+                    
                 }
-                Console.WriteLine($"Base: {ordine.BasePizza.Nome}, Impasto: {ordine.Impasto.Nome}, Aggiunte: {string.Join(", ", ordine.Aggiunte.Select(a => a.Nome))}, ID Ordine: {ordine.IdScontrino}, Prezzo Totale: {prezzoTotale}");
-            }
+           
+
         }
     }
 }
+ 
